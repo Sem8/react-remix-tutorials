6dfb6f963935d9682685378fa513f63969ea3809
Sem8 <mssemmi8@gmail.com>
2025-03-24 20:06:51 -0400

    - basic remix tutorial with loaders and actions

---
+++ remix-blog/app/root.jsx
@@ -0,0 +1,43 @@
+import { Outlet, LiveReload, Link } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title ? title : "My Remix blog"}</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
+
+function Layout({children}) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remixx
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">{children}</div>
+    </>
+  );
+}
